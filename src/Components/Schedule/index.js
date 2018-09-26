fc5faec349b7d5444bdd508a140e01780e60ec10
Dan Wolfe <dwolfe2@nd.edu>
2018-09-26 16:08:45 -0400

    schedule placeholder

--- src/Components/Schedule/index.js
+++ src/Components/Schedule/index.js
@@ -1,9 +1,42 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-const Schedule = ({match}) => {
-  console.log(match)
-  return (
-    <div className='text-content'>Information about schedule {match.params.id}</div>
-  )
+import Loading from '../Loading'
+import { schedulesReady } from '../../Modules/storeReady'
+
+const Schedule = ({match, ...props}) => {
+  if(schedulesReady(props)) {
+    // get the first (and only) matching schedule from the store
+    const schedule = props.contentReducer.schedules.filter(s => {
+      return s.sys.id === match.params.id
+    }).shift()
+
+    // render if it exists
+    if(schedule) {
+      return (
+        <React.Fragment>
+          <div className='text-content'>Information about schedule {schedule.sys.id}</div>
+          <pre style={{whiteSpace: 'pre-wrap'}}>
+            {JSON.stringify(schedule)}
+          </pre>
+        </React.Fragment>
+      )
+    }
+
+    // render message if not found
+    return (
+      <React.Fragment>
+        <div className='text-content'>No matching schedule was found.</div>
+      </React.Fragment>
+    )
+  }
+  return <Loading/>
+
 }
-export default Schedule
+
+const mapStateToProps = (state) => {
+  return {
+    ...state
+  }
+}
+export default connect(mapStateToProps)(Schedule)
