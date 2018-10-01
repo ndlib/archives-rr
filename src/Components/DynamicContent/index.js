ebfdfdb146f21c2c0485f355862e0d847af9be04
Dan Wolfe <dwolfe2@nd.edu>
2018-10-01 10:05:27 -0400

    comments and hopefully unnecessary error check

--- src/Components/DynamicContent/index.js
+++ src/Components/DynamicContent/index.js
@@ -5,19 +5,26 @@ import { connect } from 'react-redux'
 import { pagesReady } from '../../Store/storeReady'
 import Loading from '../Loading'
 
+// This component connects to the 'page' content type on Contentful and will
+// add the title and text entered there onto specific pages based on the slug
 const DynamicContent = ({slug, children, ...props}) => {
   if(pagesReady(props)) {
+    // find the page with matching slug or return a (mostly) empty page object
     let page = props.contentReducer.pages.filter(
       page => {
         return page.fields.slug === slug
       }
-    ).shift()
+    ).shift() || { fields: {} }
+
     return (
       <React.Fragment>
         <h1>{page.fields.name}</h1>
         <div className='text-content'>{page.fields.mainText}</div>
-        { children}
-        { page.fields.afterText ? <div className='text-content'>{page.fields.afterText}</div> : null}
+        { children }
+        { page.fields.afterText ?
+          <div className='text-content'>{page.fields.afterText}</div> :
+          null
+        }
       </React.Fragment>
     )
   }
