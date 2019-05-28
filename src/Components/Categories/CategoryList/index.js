bf3f98c568fe1f13f85296befdb1675dd96d35dd
Michael Kirkpatrick <mkirkpatrick@nd.edu>
2019-05-28 11:31:26 -0400

    WSE-174 Split functional categories into two columns.

--- src/Components/Categories/CategoryList/index.js
+++ src/Components/Categories/CategoryList/index.js
@@ -5,25 +5,27 @@ import { categoriesReady } from 'Store/storeReady'
 
 import Category from './Category'
 import Loading from 'Components/Shared/Loading'
+import './style.css'
 
 const CategoryList = (props) => {
   if (categoriesReady(props)) {
     // filter out categories with different categoryal leader
-    let categories = props.contentReducer.categories
+    const categories = props.contentReducer.categories
+    const maxPerColumn = Math.ceil(categories.length / 2)
 
     return (
-      <React.Fragment>
-        { categories.map(
-          category => {
-            return (
-              <Category
-                key={category.sys.id}
-                category={category}
-              />
-            )
-          })
-        }
-      </React.Fragment>
+      <div className='categoryList'>
+        <div className='categoryListColumn'>
+          { categories.slice(0, maxPerColumn).map((category) => (
+            <Category key={category.sys.id} category={category} />
+          ))}
+        </div>
+        <div className='categoryListColumn'>
+          { categories.slice(maxPerColumn, maxPerColumn * 2).map((category) => (
+            <Category key={category.sys.id} category={category} />
+          ))}
+        </div>
+      </div>
     )
   }
   return <Loading />
