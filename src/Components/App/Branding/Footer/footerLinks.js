a5f595da91a90431ae54641a66dc3a62ef565b5c
Michael Kirkpatrick <mkirkpatrick@nd.edu>
2019-05-29 13:36:09 -0400

    Modify footer links to better match library website.

--- src/Components/App/Branding/Footer/footerLinks.js
+++ src/Components/App/Branding/Footer/footerLinks.js
@@ -4,20 +4,27 @@ const FooterLinks = () => {
   return (
     <div className='footerLinks'>
       <div className='container-fluid'>
-        <div className='box'>
-          <ul>
-            <li><a href='http://archives.nd.edu' target='_blank' rel='noopener noreferrer'>Archives</a></li>
-            <li><a href='https://nd.service-now.com/nd_portal?id=sc_cat_item&sys_id=1198d67ddb4a7240de73f5161d961936&lib_list_problem=lib_list_web_content' target='_blank' rel='noopener noreferrer'>Website Feedback</a></li>
-            <li><a href='http://library.nd.edu/library-policies' target='_blank' rel='noopener noreferrer'>Library Policies</a></li>
-            <li><a href='http://librarygiving.nd.edu' target='_blank' rel='noopener noreferrer'>Library Giving</a></li>
-            <li><a href='https://nd.service-now.com/nd_portal?id=sc_cat_item&sys_id=1198d67ddb4a7240de73f5161d961936' target='_blank' rel='noopener noreferrer'>Report a Problem</a></li>
-          </ul>
-        </div>
-        <div className='box right'>
-          <ul>
-            <li><a href='http://twitter.com/ndlibraries' target='_blank' rel='noopener noreferrer'><img src='https://resources.library.nd.edu/frame/images/twitter.png' alt='Twitter' />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;NDLibraries</a></li>
-            <li><a href='https://www.facebook.com/ndlibraries/' target='_blank' rel='noopener noreferrer'><img src='https://resources.library.nd.edu/frame/images/facebook.png' alt='Facebook' />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;NDLibraries</a></li>
-          </ul>
+        <div className='row'>
+          <div className='box leftLinks'>
+            <ul>
+              <li>
+                <a href='http://archives.nd.edu' target='_blank' rel='noopener noreferrer'>University of Notre Dame Archives</a>
+              </li>
+              <li>
+                <a href='https://nd.service-now.com/nd_portal?id=sc_cat_item&sys_id=1198d67ddb4a7240de73f5161d961936' target='_blank' rel='noopener noreferrer'>Report a Problem</a>
+              </li>
+            </ul>
+          </div>
+          <div className='box rightLinks'>
+            <ul>
+              <li><a href='http://twitter.com/ndlibraries' target='_blank' rel='noopener noreferrer'>
+                <img src='https://resources.library.nd.edu/frame/images/twitter.png' alt='Twitter' /> NDLibraries
+              </a></li>
+              <li><a href='https://www.facebook.com/ndlibraries/' target='_blank' rel='noopener noreferrer'>
+                <img src='https://resources.library.nd.edu/frame/images/facebook.png' alt='Facebook' /> NDLibraries
+              </a></li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
