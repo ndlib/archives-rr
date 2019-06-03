14ebd4f2feb3b1c4b88d5b99e1b5b0ee5c6cf7d2
Michael Kirkpatrick <mkirkpatrick@nd.edu>
2019-06-03 11:14:36 -0400

    Move record navigation to its own subcomponent.

--- src/Components/RecordType/index.js
+++ src/Components/RecordType/index.js
@@ -1,33 +1,21 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
-import { Link } from 'react-router-dom'
 import Loading from 'Components/Shared/Loading'
 import { recordTypesReady } from 'Store/storeReady'
 import { searchTerms } from 'Functions/searchHelpers'
 import RecordField from './RecordField'
+import RecordTypeNav from './RecordTypeNav'
 import { displayFields, hideableFields } from 'Constants/fields'
 import NotFound from 'Components/Shared/NotFound'
 import './style.css'
 
 const RecordType = (props) => {
   if (recordTypesReady(props)) {
-    let recordType, prevRecord, nextRecord
-
-    // Get only the record types with search results OR that match the id specified
-    const filteredResults = props.contentReducer.recordTypes.filter((rec) => {
-      return rec.sys.id === props.match.params.id || rec.searchResults
-    })
     // get the first (and only) matching recordType from the store
-    const recordIndex = filteredResults.findIndex(s => {
+    const recordType = props.contentReducer.recordTypes.find(s => {
       return s.sys.id === props.match.params.id
     })
-    if (recordIndex >= 0) {
-      recordType = filteredResults[recordIndex]
-      prevRecord = filteredResults[recordIndex - 1]
-      nextRecord = filteredResults[recordIndex + 1]
-    }
-
     const terms = searchTerms(props)
 
     // render message if not found
@@ -41,18 +29,7 @@ const RecordType = (props) => {
       <div className='recordTypeDisplay'>
         <h1>
           {recordType.fields.category.fields.name}
-          <nav>
-            { prevRecord && (
-              <Link to={`/recordType/${prevRecord.sys.id}`} className='prevButton'>
-                <span className='prevArrow'>{`\u25C0`}</span>Previous
-              </Link>
-            )}
-            { nextRecord && (
-              <Link to={`/recordType/${nextRecord.sys.id}`} className='nextButton'>
-                Next<span className='nextArrow'>{`\u25B6`}</span>
-              </Link>
-            )}
-          </nav>
+          <RecordTypeNav currentId={props.match.params.id} recordTypes={props.contentReducer.recordTypes} />
         </h1>
         <div className='recordTypeFields'>
           {
