d7e11bb7dd1f86c766c785e7f095436b749b1d75
Michael Kirkpatrick <mkirkpatrick@nd.edu>
2019-06-10 14:40:34 -0400

    WSE-186 Show snippets of text in search results surrounding matched terms.

--- src/Components/Search/ListOrResults/SearchResultsList/SearchResult/index.js
+++ src/Components/Search/ListOrResults/SearchResultsList/SearchResult/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { createElement } from 'react'
 import { Link } from 'react-router-dom'
 import Highligter from 'react-highlight-words'
 import { connect } from 'react-redux'
@@ -16,6 +16,62 @@ const SearchResult = ({ result, recordTypes, ...props }) => {
       return record.sys.id === result.id
     }
   ) || { id: '', fieldsWithTerm: [], hitCount: 0 }
+
+  const makeSnippets = (textElements) => {
+    const charThreshold = 15
+    const output = []
+    let charsSinceMark = -1
+
+    textElements.forEach((el, elIndex) => {
+      let snippetText = el.props.children
+      if (el.type === 'mark') {
+        charsSinceMark = 0
+      } else {
+        let leftText = ''
+        let rightText = ''
+        const split = el.props.children.split(' ')
+
+        // Don't include a left side snippet if there was no mark (matching term) before this span
+        if (charsSinceMark > -1) {
+          // Starting from the left, start adding words until we meet the char threshold
+          let i = 0
+          while (i < split.length && charsSinceMark < charThreshold) {
+            if (i > 0) {
+              leftText += ' '
+            }
+            leftText += split[i]
+            charsSinceMark += split[i].length
+            i++
+          }
+        }
+
+        // Only do rightText if there is a highlighted term after this span.
+        if (elIndex < textElements.length - 1 && textElements[elIndex + 1].type === 'mark') {
+          let j = split.length - 1
+          while (j >= 0 && rightText.length < charThreshold) {
+            if (j < split.length - 1) {
+              rightText = ' ' + rightText
+            }
+            rightText = split[j] + rightText
+            j--
+          }
+        }
+
+        // The +1 is for a space, so we won't get ... if the only thing between leftText and rightText is a space.
+        if (leftText.length + rightText.length + 1 < el.props.children.length) {
+          // Adding space makes it a little more clear that text is omitted between two snippets
+          snippetText = (leftText && rightText) ? `${leftText} ... ${rightText}` : `${leftText}...${rightText}`
+        }
+      }
+      output.push(createElement(el.type, {
+        ...el.props,
+        children: snippetText,
+        key: el.key,
+      }))
+    })
+    return createElement('span', { children: output })
+  }
+
   return (
     <div className='result'>
       <Link to={`/recordType/${result.id}/${props.match.params.search}`}>
@@ -32,17 +88,20 @@ const SearchResult = ({ result, recordTypes, ...props }) => {
         if (field === 'recordType' || field === 'category') {
           return null
         }
+
         return (
           <div
             key={field}
             className='resultField'
           >
-            <Highligter
-              highlightClassName='term-match'
-              searchWords={searchWords}
-              autoEscape
-              textToHighlight={recordFromResult.fields[field]}
-            />
+            {
+              makeSnippets(Highligter({
+                highlightClassName: 'term-match',
+                searchWords: searchWords,
+                autoEscape: true,
+                textToHighlight: recordFromResult.fields[field],
+              }).props.children)
+            }
           </div>
         )
       })}
