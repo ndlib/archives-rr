6618a01c18887449a97243b5fcdb222410f29495
Dan Wolfe <dwolfe2@nd.edu>
2019-12-12 14:41:25 -0500

    Update to Contentful Direct in production

--- src/Constants/endpoints.js
+++ src/Constants/endpoints.js
@@ -1,2 +1,2 @@
-// export const contentfulBaseUrl = 'https://cdddliggr0.execute-api.us-east-1.amazonaws.com/archives/'
-export const contentfulBaseUrl = 'https://3r3btc956g.execute-api.us-east-1.amazonaws.com/devokta/'
+
+export const contentfulBaseUrl = 'https://zzddjr6joa.execute-api.us-east-1.amazonaws.com/archivesokta/'
