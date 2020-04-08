c7cfae8bc2a6e7dbaa7541cb6fbb7f47353dc7cb
Dan Wolfe <dwolfe2@nd.edu>
2020-04-08 10:57:14 -0400

    production endpoint

--- src/Constants/endpoints.js
+++ src/Constants/endpoints.js
@@ -1,2 +1,2 @@
 
-export const contentfulBaseUrl = 'https://ts79v1qzsc.execute-api.us-east-1.amazonaws.com/prep/'
+export const contentfulBaseUrl = 'https://406bc7ziyc.execute-api.us-east-1.amazonaws.com/prod/'
