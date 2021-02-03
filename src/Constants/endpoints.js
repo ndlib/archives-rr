1b1d2a7d8ad7dcb500a39bf9cdbbb26c0e5911cd
Michael Kirkpatrick <mkirkpatrick@nd.edu>
2021-02-03 06:24:14 -0500

    Update contentfuldirect API url.

--- src/Constants/endpoints.js
+++ src/Constants/endpoints.js
@@ -1,2 +1,2 @@
 
-export const contentfulBaseUrl = 'https://406bc7ziyc.execute-api.us-east-1.amazonaws.com/prod/'
+export const contentfulBaseUrl = 'https://fs49g1qrdg.execute-api.us-east-1.amazonaws.com/prod/'
